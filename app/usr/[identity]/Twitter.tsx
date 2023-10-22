5f8a825be0e7daa4ad1fb58b09cd4cc208fb87c5
0xVaibhav11 <vanoobcoder@gmail.com>
2023-10-22 20:43:46 +0530

    text chnaged

--- app/usr/[identity]/Twitter.tsx
+++ app/usr/[identity]/Twitter.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Dotbit = ({ id }: { id: string }) => {
-  return <div>Twitter</div>;
+const Twitter = ({ id }: { id: string }) => {
+  return (
+    <div className=" w-full min-h-screen">
+      <div className=" min-h-[50vh] w-full flex items-center px-[10%]">
+        <div className="w-[250px] h-[250px] bg-white rounded-lg"></div>
+        <div className=" w-[50%] h-full bg-black-700">
+          <p>address: 0xlllwlwlwlwlwlwlwll</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Dotbit;
+export default Twitter;
